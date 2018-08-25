cf9e4c7f1199b04832c22cb2d45daec21ad7303e
stephenho123 <42186223+stephenho123@users.noreply.github.com>
2018-08-24 19:22:16 -0500

    created Board class created

---
+++ Board.py
@@ -0,0 +1,56 @@
+class Board:
+    def __init__(self):
+        self.theBoard = [' '] * 10
+    def drawBoard(self):
+        # This function prints out the board that it was passed.
+        board = self.theBoard
+        # "board" is a list of 10 strings representing the board (ignore index 0)
+        print('   |   |')
+        print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+        print('   |   |')
+    def makeMove(self, letter, move):
+        board = self.theBoard
+        board[move] = letter
+
+    def isWinner(self, le):
+        bo = self.theBoard
+        # Given a board and a player's letter, this function returns True if that player has won.
+        # We use bo instead of board and le instead of letter so we don't have to type as much.
+        return ((bo[7] == le and bo[8] == le and bo[9] == le) or  # across the top
+                (bo[4] == le and bo[5] == le and bo[6] == le) or  # across the middle
+                (bo[1] == le and bo[2] == le and bo[3] == le) or  # across the bottom
+                (bo[7] == le and bo[4] == le and bo[1] == le) or  # down the left side
+                (bo[8] == le and bo[5] == le and bo[2] == le) or  # down the middle
+                (bo[9] == le and bo[6] == le and bo[3] == le) or  # down the right side
+                (bo[7] == le and bo[5] == le and bo[3] == le) or  # diagonal
+                (bo[9] == le and bo[5] == le and bo[1] == le))  # diagonal
+
+    def getBoardCopy(self):
+        board = self.theBoard
+        # Make a duplicate of the board list and return it the duplicate.
+        dupeBoard = []
+
+        for i in board:
+            dupeBoard.append(i)
+
+        return dupeBoard
+
+    def isSpaceFree(self, move):
+        board = self.theBoard
+        # Return true if the passed move is free on the passed board.
+        return board[move] == ' '
+
+    def isBoardFull(self):
+        # Return True if every space on the board has been taken. Otherwise return False.
+        for i in range(1, 10):
+            if self.isSpaceFree(i):
+                return False
+        return True
